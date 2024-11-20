b82d98b7e6073518e4d8b0710e8ffe5010cb3a3e
LahkLeKey <LahkLeKey@gmail.com>
2024-11-20 15:43:12 -0600

    Update 13.roman-to-integer.ts

--- 13.roman-to-integer.ts
+++ 13.roman-to-integer.ts
@@ -6,7 +6,36 @@
 
 // @lc code=start
 function romanToInt(s: string): number {
-    
+    const mapRomanToString : Record<string, number> = {
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XC: 90,
+        XL: 40,
+        L: 50,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000,
+    }
+    let total = 0;
+    for (let index = 0; index < s.length; index++) {
+        const forwardLookup  = s.slice(index, index + 2);
+        const isSpecialCase = (lookup: string): boolean => {
+            return ['IV', 'IX', 'XL', 'XC', 'CD', 'CM'].includes(lookup);
+        };
+
+        if (isSpecialCase(forwardLookup)) {
+            total += mapRomanToString[forwardLookup];
+            index++;
+        } else {
+            total += mapRomanToString[s[index]];
+        }
+    }
+    return total;
 };
 // @lc code=end
 
